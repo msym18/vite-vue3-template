ceb94522b413c2c6b4a3c0857caf02428d902b1f
msym18 <msym18@users.noreply.github.com>
2022-11-02 10:00:08 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 85a4a2ff5a75aed9362d4786503f55040fadc70b

---
+++ assets/demo1.0a8f2c33.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as t}from"./index.c224b42b.js";const o={setup(){return{}}};function c(s,a,n,p,_,d){return r(),t("div")}var u=e(o,[["render",c]]);export{u as default};
--- assets/demo1.91cd685c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c as t}from"./index.d381be23.js";const o={setup(){return{}}};function c(s,a,n,p,_,d){return r(),t("div")}var u=e(o,[["render",c]]);export{u as default};
